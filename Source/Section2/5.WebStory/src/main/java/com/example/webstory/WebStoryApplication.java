41f0a7d3561bddddd7b252abbfa3a0cc9891d581
DESKTOP-RTMQGFQ\USER <good.dhkim@gmail.com>
2022-12-24 20:32:29 +0900

    File Up

---
+++ Source/Section2/5.WebStory/src/main/java/com/example/webstory/WebStoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.webstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoryApplication.class, args);
+    }
+
+}
---
+++ Source/Section2/5.WebStory/src/test/java/com/example/webstory/WebStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
