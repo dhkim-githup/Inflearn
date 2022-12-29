d1ba7f41481c25f649d309704874c973343d9b9a
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2022-12-29 17:54:02 +0900

    File Up

--- Sailing_Dev/src/main/java/com/boot/sailing_dev/v1/controller/Member.java
+++ Sailing_Dev/src/main/java/com/boot/sailing_dev/v1/controller/Member.java
@@ -1,13 +1,19 @@
 package com.boot.sailing_dev.v1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class Member {
 
     @RequestMapping("/v1/member")
-    public String doMember(){
+    public String doMember(Model model){
         return "/v1/member/member";
     }
 
