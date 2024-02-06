fbae07ab86cc85b24ea6c77f4bea757b6fdd679e
NBC10160027\NBC10160027 <good.dhkim@gmail.com>
2024-02-06 17:53:10 +0900

    File Up

--- YouMeSecurity/src/main/java/com/example/security/config/SecurityConfig.java
+++ YouMeSecurity/src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.security.config;
 
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,16 +32,38 @@ public class SecurityConfig {
                 );
 
         http
-                .formLogin((auth) -> auth.loginPage("/login")
+                .formLogin((auth) -> auth
+                        .loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
 
         http
-                .csrf((auth) -> auth.disable());
+                .sessionManagement((auth) -> auth
+                .maximumSessions(1) // 하나의 아이디에 대한 다중 로그인 허용 개수
+                .maxSessionsPreventsLogin(true))// 다중 로그인 개수를 초과하였을 경우 처리 방법- true : 초과시 새로운 로그인 차단- false : 초과시 기존 세션 하나 삭제
+                ;
+
+        http
+                .sessionManagement((auth) -> auth
+                .sessionFixation().changeSessionId()); //
+
+        //http
+          //      .csrf((auth) -> auth.disable());
+
 
         return http.build();
     }
 
+    /* 동시성 이슈
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }// Register HttpSessionEventPublisher
 
+    @Bean
+    public static ServletListenerRegistrationBean httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
+    }
+    */
 }
