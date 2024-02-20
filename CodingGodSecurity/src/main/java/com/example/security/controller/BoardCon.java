a37098dadfa2a53315a80100704cadb2c6b62de0
NBC10160027\NBC10160027 <good.dhkim@gmail.com>
2024-02-20 18:30:23 +0900

    File Up

--- CodingGodSecurity/src/main/java/com/example/security/controller/BoardCon.java
+++ CodingGodSecurity/src/main/java/com/example/security/controller/BoardCon.java
@@ -5,6 +5,10 @@ import com.example.security.repository.BoardRepo;
 import com.example.security.validator.BoardValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,9 +27,23 @@ public class BoardCon {
     private BoardRepo boardRepo;
 
     @GetMapping("/list")
-    public String doList(Model model){
+    public String doList(Model model,
+                         @PageableDefault(size = 4) Pageable pageable,
+                         @RequestParam(required = false, defaultValue = "") String searchText){
 
-        List<Board> boardList =  boardRepo.findAll();
+        //List<Board> boardList =  boardRepo.findAll();
+        //Page<Board> boardList =  boardRepo.findAll(PageRequest.of(0,2));
+
+        //http://localhost:8080/board/list?page=0&size=3
+        //Page<Board> boardList =  boardRepo.findAll(pageable);
+        Page<Board> boardList =  boardRepo.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+        int startPage = Math.max(1,boardList.getPageable().getPageNumber()-4);
+        int endPage   = Math.min(boardList.getTotalPages(),boardList.getPageable().getPageNumber()+4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         model.addAttribute("board", boardList);
 
         return "board/list";
--- CodingGodSecurity/src/main/java/com/example/security/repository/BoardRepo.java
+++ CodingGodSecurity/src/main/java/com/example/security/repository/BoardRepo.java
@@ -1,9 +1,18 @@
 package com.example.security.repository;
 
 import com.example.security.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepo extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
